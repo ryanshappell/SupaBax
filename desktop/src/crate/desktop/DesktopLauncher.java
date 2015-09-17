775db1b50522ec1bed7522202ab0999ee42c523d
Aaron Shappell <crumster88@gmail.com>
2015-09-17 18:46:31 +0300

    Fix DesktopLauncher

--- desktop/src/crate/desktop/DesktopLauncher.java
+++ desktop/src/crate/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package crate.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import crate.SupaBox;
+
+import crate.SupaBax;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SupaBox(), config);
+		new LwjglApplication(new SupaBax(), config);
 	}
 }
